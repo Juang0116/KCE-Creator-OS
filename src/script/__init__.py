4f03bee4f617a84c7eae0c40598da3eebf20cff1
Juan Andres Garzon <knowing.cultures.enterprise@gmail.com>
2026-09-07 21:21:15 -0500

    chore: export Script models

---
+++ src/script/__init__.py
@@ -0,0 +1,27 @@
+from .models import (
+    Script,
+    ScriptSource,
+    ScriptTarget,
+    ScriptMetadata,
+    ScriptHook,
+    ScriptIntroduction,
+    ScriptClaim,
+    ScriptSection,
+    ScriptConclusion,
+    ScriptCTA,
+    ScriptProcessing,
+)
+
+__all__ = [
+    "Script",
+    "ScriptSource",
+    "ScriptTarget",
+    "ScriptMetadata",
+    "ScriptHook",
+    "ScriptIntroduction",
+    "ScriptClaim",
+    "ScriptSection",
+    "ScriptConclusion",
+    "ScriptCTA",
+    "ScriptProcessing",
+]
